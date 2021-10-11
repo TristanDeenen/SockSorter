f0040477aec0ce71d70a0cf4364bc2486919d253
AINeerhoff <a.i.neerhoff@student.tue.nl>
2021-10-11 15:04:40 +0200

    Color sensor updated+ servo

--- Color_Sensor/Color_Sensor/Color_Sensor.ino
+++ Color_Sensor/Color_Sensor/Color_Sensor.ino
@@ -3,7 +3,10 @@
 #define S2 6
 #define S3 7
 #define sensorOut 8
-int frequency = 0;
+
+int red_frequency = 0;
+int green_frequency = 0;
+int blue_frequency = 0;
 
 void setup() {
   pinMode(S0, OUTPUT);
@@ -20,42 +23,56 @@ void setup() {
 }
 
 void loop() {
-  // Setting red filtered photodiodes to be read
+  red_frequency = readRedFrequency();
+  delay(100);
+  green_frequency = readGreenFrequency();
+  delay(100);
+  blue_frequency = readBlueFrequency();
+  delay(100);
+  
+  Serial.print("R= ");//printing name
+  Serial.print(red_frequency);//printing RED color frequency
+  Serial.print("  ");
+  
+  Serial.print("G= ");//printing name
+  Serial.print(green_frequency);//printing RED color frequency
+  Serial.print("  ");
+  
+  Serial.print("B= ");//printing name
+  Serial.print(blue_frequency);//printing RED color frequency
+  Serial.println("  ");
+
+}
+
+int readRedFrequency(){
+  int frequency = 0;
   digitalWrite(S2,LOW);
   digitalWrite(S3,LOW);
   // Reading the output frequency
   frequency = pulseIn(sensorOut, LOW);
-  //Remaping the value of the frequency to the RGB Model of 0 to 255
-  frequency = map(frequency, 25,72,255,0);
-  // Printing the value on the serial monitor
-  Serial.print("R= ");//printing name
-  Serial.print(frequency);//printing RED color frequency
-  Serial.print("  ");
-  delay(100);
-  
-  // Setting Green filtered photodiodes to be read
+  //Remapping the value of the frequency to the RGB Model of 0 to 255
+  frequency = map(frequency, 20,177,255,0);
+  return frequency;
+}
+
+int readGreenFrequency(){
+  int frequency = 0;
   digitalWrite(S2,HIGH);
   digitalWrite(S3,HIGH);
   // Reading the output frequency
   frequency = pulseIn(sensorOut, LOW);
   //Remaping the value of the frequency to the RGB Model of 0 to 255
-  frequency = map(frequency, 30,90,255,0);
-  // Printing the value on the serial monitor
-  Serial.print("G= ");//printing name
-  Serial.print(frequency);//printing RED color frequency
-  Serial.print("  ");
-  delay(100);
-  
-  // Setting Blue filtered photodiodes to be read
+  frequency = map(frequency, 30,250,255,0);
+  return frequency;
+}
+
+int readBlueFrequency(){
+  int frequency = 0;
   digitalWrite(S2,LOW);
   digitalWrite(S3,HIGH);
   // Reading the output frequency
   frequency = pulseIn(sensorOut, LOW);
   //Remaping the value of the frequency to the RGB Model of 0 to 255
-  frequency = map(frequency, 25,70,255,0);
-  // Printing the value on the serial monitor
-  Serial.print("B= ");//printing name
-  Serial.print(frequency);//printing RED color frequency
-  Serial.println("  ");
-  delay(100);
+  frequency = map(frequency, 35,200,255,0);
+  return frequency;
 }
