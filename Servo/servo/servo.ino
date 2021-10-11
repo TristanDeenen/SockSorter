16c7cfdc333b7adfd2232c842800c46906f78bf5
AINeerhoff <a.i.neerhoff@student.tue.nl>
2021-10-11 17:06:24 +0200

    Update servo.ino

--- Servo/servo/servo.ino
+++ Servo/servo/servo.ino
@@ -1,18 +1,7 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
 
 void setup() {
   myservo.attach(11);  // attaches the servo on pin 9 to the servo object
@@ -20,6 +9,6 @@ void setup() {
 
 void loop() {
 
-    myservo.write(0);              // tell servo to go to position in variable 'pos'
+    myservo.write(0);              //tell servo to go either forwards or backwards by sending a value between 0 to 180
  
 }
