40879dfd1d5034c24f96e918b2ab4ba504e7039b
TristanDeenen <72860085+TristanDeenen@users.noreply.github.com>
2021-10-11 11:35:37 +0200

    Added high level implementation/specification
    SockScanner and SockComparer are now more worked out. WARNING, untested code, might contain bugs!

--- SockComparer/SockComparer.ino
+++ SockComparer/SockComparer.ino
@@ -1,11 +1,24 @@
 /** This file stores the sock-ids and couples them to a basket. It also compares sock-ids to see check if there is a match.
-If there is a match,  then it outputs the corresponding basket. */
-int x = 5;
+If there is a match, then it outputs the corresponding basket. */
 
+SockScanner sockScanner;
+SockScanner[] sockIDs; // TODO Might have a problem since arrays do not have flexible length. Consider using lists, or just initialize at max amount of baskets.
 void setup() {
 //TODO
 }
 
+void scanSock() {
+  sockScanner.createSockID(sockScanner.readRGB(), sockScanner.readIR());
+}
+
+void addSockID(int n) {
+  SockIDS[n] = sockScanner.getSockID();
+}
+
+int compareSocks(SockScanner[] sockID) {
+  //TODO create epic comparison algorithm to compare socks
+  return 0; //Placeholder, should return the right basket.
+}
 void loop() {
 //TODO
 }
--- SockScanner/SockScanner.ino
+++ SockScanner/SockScanner.ino
@@ -1,11 +1,33 @@
 /** This file takes the input from the RGB-scanner and the IR-scanner
 and converts that data to a sock-id. Then it outputs the id.*/
-int x = 5;
+
+//TODO Object type to be decided, int is placeholder
+int sockID = 69;
 
 void setup() {
 //TODO
 }
 
+int[] readRGB() {
+  //TODO
+  return []
+}
+
+//TODO input type not clear yet
+void readIR() {
+  //TODO
+}
+
+// TODO Need to decide which data type sock-id will be for function type
+// TODO Input todo should be IR sensor
+void createSockID(int[] rgb, int todo) {
+  //TODO
+}
+
+// TODO Object type sockID to be decided, int placeholder
+int getSockID() {
+  return sockID;
+}
 void loop() {
 //TODO
 }
