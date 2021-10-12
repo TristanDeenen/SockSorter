33ef30df95434ff923a3aea81a9779203e20bb2a
AINeerhoff <a.i.neerhoff@student.tue.nl>
2021-10-12 10:16:34 +0200

    update IR

--- IR_Sensor/IR_Sensor.ino
+++ IR_Sensor/IR_Sensor.ino
@@ -1,17 +1,14 @@
 int IRSensor = 2; // connect ir sensor to arduino pin 2
 
-
-
-
 void setup() 
 {
   pinMode (IRSensor, INPUT); // sensor pin INPUT
-   Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  int statusSensor = digitalRead (IRSensor);
+  int statusSensor = digitalRead (IRSensor);  // 1 means no object in front, 0 means object in front
   
 //  if (statusSensor == 1){
 //    Serial.println("too close");
@@ -23,5 +20,5 @@ void loop()
 //  }
 //  delay(200);
 Serial.println(statusSensor);
-delay(200);
+delay(200);                                   // delay to stabilize the sensor 
 }
