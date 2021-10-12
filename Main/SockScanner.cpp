f5126c9cd1f3b00fbda488a0b56663c1d2aeb08d
MoshaLangerak <82883699+MoshaLangerak@users.noreply.github.com>
2021-10-12 10:40:03 +0200

    Update SockScanner.cpp

--- Main/SockScanner.cpp
+++ Main/SockScanner.cpp
@@ -1,15 +1,15 @@
 #include "Sockscanner.h"
 #include "Color_Sensor.h"
 
-int[] readRGB() {
+int[] readRGB(int S2, int S3, int sensorOut) {
   //TODO
   // Declaring array for the RGB value
   int RGBValue[3];
 
   // Using the color sensor to get values for the frequency
-  RGBValue[0] = readRedFrequency();
-  RGBValue[1] = readGreenFrequency(); 
-  RGBValue[2] = readBlueFrequency();
+  RGBValue[0] = readRedFrequency(S2, S3, sensorOut);
+  RGBValue[1] = readGreenFrequency(S2, S3, sensorOut); 
+  RGBValue[2] = readBlueFrequency(S2, S3, sensorOut);
 
   return RGBValue;
 }
