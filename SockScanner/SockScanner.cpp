7d912e52d0b022dfe001833c2b88bf457e16ec7e
TristanDeenen <72860085+TristanDeenen@users.noreply.github.com>
2021-10-12 10:15:50 +0200

    SockScanner converted

---
+++ SockScanner/SockScanner.cpp
@@ -0,0 +1,20 @@
+int[] readRGB() {
+  //TODO
+  return []
+}
+
+//TODO input type not clear yet
+void readIR() {
+  //TODO
+}
+
+// TODO Need to decide which data type sock-id will be for function type
+// TODO Input todo should be IR sensor
+void createSockID(int[] rgb, int todo) {
+  //TODO
+}
+
+// TODO Object type sockID to be decided, int placeholder
+int getSockID() {
+  return sockID;
+}
---
+++ SockScanner/Sockscanner.h
@@ -0,0 +1,10 @@
+#ifndef SOCK_SCANNER_H
+#define SOCK_SCANNER_H
+#include <Arduino.h>
+
+int[] readRGB();
+void readIR()();
+void createSockID(int[] rgb, int todo);
+int getSockID();
+
+#endif
