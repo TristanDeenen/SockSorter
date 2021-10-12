f91c0455b3e6a32be53c88b5f9f0796e7bed17f0
TristanDeenen <72860085+TristanDeenen@users.noreply.github.com>
2021-10-12 10:53:13 +0200

    Converted IR_Sensor

---
+++ Main/IR_Sensor.cpp
@@ -0,0 +1,14 @@
+#include "IR_Sensor.h"
+
+//WARNING, now prints values for IR, comment out per function to disable!
+
+bool sockUnderSensor(int irSensorPin) {
+  int statusSensor = digitalRead (IRSensor);  // 1 means no object in front, 0 means object in front
+  Serial.println(statusSensor);
+  delay(200);                                   // delay to stabilize the sensor
+  if (statusSensor == 1) {
+    return false;
+  } else {
+    return true;
+  }
+}
---
+++ Main/IR_Sensor.h
@@ -0,0 +1,7 @@
+#ifndef IR_SENSOR_H
+#define IR_SENSOR_H
+#include <Arduino.h>
+
+bool sockUnderSensor(int irSensorPin);
+
+#endif
