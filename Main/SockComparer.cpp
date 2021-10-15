93384747f41956a478e069f8fccce805be131914
MoshaLangerak <82883699+MoshaLangerak@users.noreply.github.com>
2021-10-15 16:18:04 +0200

    Sockcomparer update
    Vector norm, dot product en cosine similarity

--- Main/SockComparer.cpp
+++ Main/SockComparer.cpp
@@ -4,15 +4,26 @@ If there is a match, then it outputs the corresponding basket. */
 #include "SockComparer.h"
 #include "SockScanner.h"
 
-int dotProduct(){
-  for(unsigned i = 0; i < vec1.ncols(); ++i){
-  ret += vec1[i] * vec2[i];
-}
+int dotProduct(int vec1[], int vec2[]){
+    //return dot product of an array
+    int i = 0;
+    int result = NULL;
+
+    while(i < 3){
+    result += vec1[i] * vec2[i];
+    i++;
+    }
+
+    return result;
+  }
+
+int vectorNorm(int vec[]){
+  //return Euclidian norm of an array
+  return sqrt(pow(vec[0], 2) + pow(vec[1], 2) + pow(vec[2], 2));
 }
 
-int compareSocks(int sockID1, int sockID2) {
-  //TODO create epic comparison algorithm to compare socks
-
+int compareSocks(int RGBValue1[], int RGBValue2[]){
+  //epic comparison algorithm to compare socks
+  int comparisonMeasure = dotProduct(RGBValue1, RGBValue2)/(vectorNorm(RGBValue1)*vectorNorm(RGBValue2)); 
   return comparisonMeasure;
 }
-
--- Main/SockComparer.h
+++ Main/SockComparer.h
@@ -1,9 +1,9 @@
 #ifndef SOCK_COMPARER_H
 #define SOCK_COMPARER_H
 #include <Arduino.h>
-/*
-bool scanSock(SockScanner sockScanner);
-void addSockID(SockScanner sockScanner, int n);
-int compareSocks(SockScanner[] sockID);
-*/
+
+int dotProduct(int vec1[], int vec2[]);
+int vectorNorm(int vec[]);
+int compareSocks(int RGBValue1[], int RGBValue2[]);
+
 #endif
