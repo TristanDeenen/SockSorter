1b52980aa8e99779f70bd7b34b0c97d9ee3c52a4
TristanDeenen <72860085+TristanDeenen@users.noreply.github.com>
2021-10-15 14:14:14 +0200

    intermediate main push

--- Main/Main.ino
+++ Main/Main.ino
@@ -14,42 +14,28 @@
 #define S3 7
 #define sensorOut 8
 #define irPin 69 // TODO CHANGE
+#define rollerband_speed 75
 
 void setup() {
   Serial.begin(9600);
   setupColorSensor(S0, S1, S2, S3, sensorOut);
   pinMode (irPin, INPUT);
+  //TODO slightly inefficient solution, might change later
+  rollerbandToggle(true); // WARNING FUNCTION INCORRECT: SPEED IS SET AT 0 AND TOGGLE FEATURE NOT IMPLEMENTED YET 
 }
 
+ // Main loop after setup is finished
  void loop(){
- readRedFrequency(S2, S3, sensorOut);
- readGreenFrequency(S2, S3, sensorOut);
- readBlueFrequency(S2, S3, sensorOut);
 
- Serial.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
- int x[] = {1, 2, 3};
- int y[3] = {};
- readRGB(y, S2, S3, sensorOut);
- Serial.println(String(y[0]) + y[1] + y[2] + "BELANGRIJK");
- //int arr[] = readRGB(S2, S3, sensorOut);
- //Serial.println(arr[0] + arr[1] + arr[2] + "BELANGRIJK");
+  //Check for a sock
+  if sockUnderSensor(irPin){
+    //Get measurements
+    createSockID(); //TODO wait on implementation to specify function more
+    compareSocks(); //TODO function does not exist yet
 
-
-
-
- //Main loop after setup is finished
- //Initilise triple array 
- //Check for a sock
- if sockUnderSensor(irPin){
-   //initialise new sock id
-
-   //Get measurements
-   createSockID(int *ar, bool sockCheck, int measurements)
-
-   //Comparesock_id
+    // Move the correct bin into output position
+   // moveBin();
    
-
  }
- delay(5000)
 
  }
