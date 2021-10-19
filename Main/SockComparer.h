2f308755d8e8151fc9b62f353fc1129eb6f758b7
AINeerhoff <a.i.neerhoff@student.tue.nl>
2021-10-19 08:41:49 +0200

    bugfix

--- Main/SockComparer.h
+++ Main/SockComparer.h
@@ -5,9 +5,14 @@
 int dotProduct(int vec1[], int vec2[]);
 int vectorNorm(int vec[]);
 int cosineSimilarity(int RGBValue1[], int RGBValue2[]);
+<<<<<<< Updated upstream
 int sockComparer(int *ar, int bins, int measurements)
 double calculateAverage(int a[]);
 int lastSockIDfinder(int *ar);
 int sockMatcher(int lastSockID, int avgcosSimilarities, int treshold);
+=======
+int sockComparer(int *ar, int bins, int measurements);
+double calculateAverage(int a[], int n);
+>>>>>>> Stashed changes
 
 #endif
